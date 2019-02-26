4eb344d73696cd32e969bbe9f0e8fee52bce002c
苏杨 <suyang_5070@126.com>
2019-02-26 23:55:03 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {Button} from 'antd-mobile'
 import App from './components/app/index';
 
 ReactDOM.render(<App/> ,document.getElementById('root'));
